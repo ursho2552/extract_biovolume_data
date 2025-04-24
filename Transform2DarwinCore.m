12f8f0debb5be3e5886a0d92f458a87330703285
ursho2552 <urs.hofmann@usys.ethz.ch>
2025-04-24 19:46:11 +0200

    Removed extre %%

--- Transform2DarwinCore.m
+++ Transform2DarwinCore.m
@@ -170,7 +170,7 @@ occurrenceID = SampleID + '_' + Latitude + '_' + Longitude + '_' + Event_date + 
 
 TARAregevents_file = '/net/kryo/work/ursho/PhD/Projects/BlueCloud/Imaging_data/Biovolume_data/TARA_reg_events.csv';
 TARAregevents = importfile_TARAregevents(TARAregevents_file, 2, 3265);
-%%
+
 % Identify missasigned measurements
 [C, ia, ic] = unique([TARA_station, Latitude, Longitude, Instrument], 'rows');
 instruments = C(:,4);
